401ad04768907a1edd1d141a47f05f22fa8af738
9g9g9g <ktg4933@gmail.com>
2025-06-09 21:04:32 +0900

    야옹이와 수프 ver.1 내용 추가

---
+++ FileName.c
@@ -0,0 +1,160 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<Windows.h>
+
+
+int main(void)
+{
+
+	int soup = 0, com = 2, a, dice, catpos = 1, before = 1, b;//soup:수프의 개수, com:집사와의 관계, a:0 or 1, dice:주사위 눈 catpos:고양이 위치, before:고양이 전 위치, 수프 종류
+	char cat[100];//고양이 이름
+	srand((unsigned int)time(NULL));
+	printf("****야옹이와 수프****\n\n");
+	printf("      /\\_/\\\n /\\  / ° ° \\\n//\\\\ \\~(*)~/\n`  \\/   ^ /\n   | \\|| ||\n   \\ '|| ||\n    \\)()-())\n\n");
+	printf("야옹이의 이름을 지어 주세요: ");
+	gets_s(cat, sizeof(cat));
+	printf("야옹이의 이름은 %s입니다.\n", cat);
+	Sleep(1000);
+	system("cls");//오프닝
+
+	Sleep(500);
+
+	while (1) {
+		printf("==================== 현재상태 ====================\n");
+		printf("현재까지 만든 수프: %d개\n", soup);
+		printf("집사와의 관계(0~4): %d\n  ", com);
+		switch (com) {
+		case 0:
+			printf("곁에 오는 것조차 싫어합니다.\n"); break;
+		case 1:
+			printf("간식 자판기 취급입니다.\n"); break;
+		case 2:
+			printf("그럭저럭 쓸 만한 집사입니다.\n"); break;
+		case 3:
+			printf("훌륭한 집사로 인정 받고 있습니다.\n"); break;
+		case 4:
+			printf("집사 껌딱지입니다.\n"); break;
+		}
+		printf("==================================================\n\n");//상태
+
+		Sleep(500);
+
+		printf("%s 이동: 집사와 친밀할수록 냄비 쪽으로 갈 확률이 높아집니다.\n주사위 눈이 %d 이상이면 냄비 쪽으로 이동합니다.\n", cat, 6 - com);
+		dice = rand() % 6 + 1;
+		printf("주사위를 굴립니다. 또르륵...\n");
+		printf("%d가 나왔습니다!\n", dice);
+		if (dice >= 6 - com) {
+			printf("냄비 쪽으로 이동합니다.\n\n");
+			if (catpos != 4) {
+				catpos++;
+			}
+		}
+		else if (dice < 6 - com) {
+			printf("집 쪽으로 이동합니다.\n\n");
+			if (catpos > 1) {
+				catpos--;
+			}
+		}
+		//고양이 이동
+		if (catpos == 4) {
+			b = rand() % 3 + 1;
+			switch (b) {
+			case 1:printf("%s이(가) 감자 수프를 만들었습니다!\n", cat); break;
+			case 2:printf("%s이(가) 양송이 수프를 만들었습니다!\n", cat); break;
+			case 3:printf("%s이(가) 브로콜리 수프를 만들었습니다!\n", cat); break;
+			}
+			soup++;
+			printf("현재까지 만든 수프: %d개\n\n", soup);
+		}
+		else if (catpos == 1) {
+			printf("%s은(는) 자신의 집에서 편안함을 느낍니다.\n\n", cat);
+		}
+		//고양이 행동 
+		Sleep(500);
+
+		printf("######\n#H  B#\n");
+		if (catpos > before) {
+			switch (catpos) {
+			case 2: printf("#.C  #\n"); before++; break;
+			case 3: printf("# .C #\n"); before++; break;
+			case 4: printf("#  .C#\n"); before++; break;
+			}
+		}
+		else if (catpos < before) {
+			switch (catpos) {
+			case 1: printf("#C.  #\n"); before--; break;
+			case 2: printf("# C. #\n"); before--; break;
+			case 3: printf("#  C.#\n"); before--; break;
+			}
+		}
+		else if (catpos == before) {
+			switch (catpos) {
+			case 1: printf("#C   #\n"); break;
+			case 4: printf("#   C#\n"); break;
+			}
+		}
+
+
+		printf("######\n\n");
+		// 뱡만들기
+		Sleep(500);
+
+		printf("어떤 상호작용을 하시겠습니까?   0. 아무것도 하지 않음  1. 긁어 주기\n");
+		while (1) {
+			printf(">> ");
+			scanf_s("%d", &a);
+			if (a == 0 || a == 1) {
+				break;
+			}
+		}
+		switch (a) {
+		case 0:
+			printf("아무것도 하지 않았습니다.\n4/6의 확률로 친밀도가 떨어집니다.\n주사위를 굴립니다. 또르륵...\n");
+			dice = rand() % 6 + 1;
+			printf("%d이(가) 나왔습니다!\n", dice);
+			switch (dice) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				if (com > 0) {
+					com -= 1;
+				}
+				printf("친밀도가 떨어집니다.\n현재 친밀도: %d\n", com);
+				break;
+			case 5:
+			case 6:
+				printf("다행히 친밀도가 떨어지지 않았습니다.\n현재 친밀도: %d\n", com);
+				break;
+			}
+			break;
+			//상호작용 0 누를때
+		case 1:
+			printf("%s의 턱을 긁어주었습니다.\n2/6의 확률로 친밀도가 높아집니다.\n주사위를 굴립니다. 또르륵...\n", cat);
+			dice = rand() % 6 + 1;
+			printf("%d이(가) 나왔습니다.\n", dice);
+			switch (dice) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				printf("친밀도는 그대로입니다.\n현재 친밀도: %d\n", com);
+				break;
+			case 5:
+			case 6:
+				if (com < 4) {
+					com += 1;
+				}
+				printf("친밀도가 높아집니다.\n현재 친밀도: %d\n", com);
+			}
+		}
+		//상호작용 1 누를떄
+
+		Sleep(2500);
+		system("cls");//지우고 상태부터 다시
+	}
+	return 0;
+}
+
+
